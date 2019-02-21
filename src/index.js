141211ece29818b15b1ceb78b0cf5132053d61c7
Steven Fisher <steven.fisher22@gmail.com>
2019-02-20 23:24:47 -0600

    added StorePicker.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import StorePicker from "./components/StorePicker"
+import './css/style.css'
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<StorePicker />, document.getElementById('root'));
